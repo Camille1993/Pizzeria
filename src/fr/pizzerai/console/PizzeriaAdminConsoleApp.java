9857647be4b730948db31b8f4b97a2f07e54d39a
Camille <camille.hautois@gmail.com>
2019-02-07 14:14:32 +0100

    ajout du DAO

--- src/fr/pizzerai/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzerai/console/PizzeriaAdminConsoleApp.java
@@ -3,32 +3,33 @@ package fr.pizzerai.console;
 import java.util.Scanner;
 
 import fr.pizzeria.model.Pizza.Pizza;
+import fr.pizzeria.dao.IPizzaDao;
+import fr.pizzeria.dao.PizzaMemDao;
 
 public class PizzeriaAdminConsoleApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		//tableau pizza		
-		Pizza pep = new Pizza (0, "PEP", "Pépéroni", 12.50);
-		Pizza mar = new Pizza (1, "MAR", "Margherita", 14.00);
-		Pizza rein = new Pizza (2, "REIN", "La reine", 11.50);
-		Pizza fro = new Pizza (3, "FRO", "La 4 fromages", 12.00);
-		Pizza car = new Pizza (4, "CAR", "La carnivore", 12.50);
-		Pizza sav = new Pizza (5, "SAV", "La savoyarde", 13.00);
-		Pizza ori = new Pizza (6, "ORI", "L'orientale", 13.50);
-		Pizza ind = new Pizza (7, "IND", "L'indienne", 14.00);
-
-		Pizza[] tableauPizza = {pep, mar, rein, fro, car, sav, ori, ind};
 		Scanner choiceMenu = new Scanner(System.in);
+
+		//tableau pizza	
+		IPizzaDao pizzaDao = new PizzaMemDao();
+		pizzaDao.saveNewPizza(new Pizza (0, "PEP", "Pépéroni", 12.50));
+		pizzaDao.saveNewPizza(new Pizza (1, "MAR", "Margherita", 14.00));
+		pizzaDao.saveNewPizza(new Pizza (2, "REIN", "La reine", 11.50));
+		pizzaDao.saveNewPizza(new Pizza (3, "FRO", "La 4 fromages", 12.00));
+		pizzaDao.saveNewPizza(new Pizza (4, "CAR", "La carnivore", 12.50));
+		pizzaDao.saveNewPizza(new Pizza (5, "SAV", "La savoyarde", 13.00));
+		pizzaDao.saveNewPizza(new Pizza (6, "ORI", "L'orientale", 13.50));
+		pizzaDao.saveNewPizza(new Pizza (7, "IND", "L'indienne", 14.00));
+		//Méthode pour créer tableau pizza
 		
 
 		int choice =0;
 		String code;
 		String nom;
 		double prix;
-		
+
 		//afficher le menu		
 		while (true) {
 			System.out.println("*******Bienvenue sur le service administration******");
@@ -40,54 +41,48 @@ public class PizzeriaAdminConsoleApp {
 
 			choice = choiceMenu.nextInt();
 			choiceMenu.nextLine();
-			//options		
+
+			//options
+
 			if (choice == 1) {
 				System.out.println("Liste des pizzas");
-				//liste pizza suite option 1
+
+				//méthode pour afficher pizza
+
+				Pizza[] tableauPizza = pizzaDao.findAllPizzas();
+
 				for (int i =0; i<tableauPizza.length; i++) {
 					System.out.println(tableauPizza[i]);
 				}
+
 			}else if (choice == 2) {
+
 				//declarer une nouvelle pizza				
 				System.out.println("Ajouter une nouvelle pizza");
-				
+
 				System.out.println("Veuillez saisir le code : ");
 				code = choiceMenu.nextLine();
-				
+
 				System.out.println("Veuillez saisir le nom (sans espace) : ");
 				nom = choiceMenu.nextLine();
-				
+
 				System.out.println("Veuillez saisir le prix : ");
 				prix = choiceMenu.nextDouble();
 
-				// Creer un tableau plus grand de 1		
-				int newLength = tableauPizza.length + 1;
-				Pizza[] newTableauPizza = new Pizza[newLength];
-				
-				// Copier les anciennes valeurs
-					
-				for(int i = 0; i< tableauPizza.length; i++){
-					newTableauPizza[i] = tableauPizza[i];	
-				}
-				//Ajouter la nouvelle pizza
-				
-				int id = newLength - 1;
-				newTableauPizza[tableauPizza.length] = new Pizza(id, code, nom, prix);
-				
-				//remplacer la liste precedente
-				tableauPizza = newTableauPizza;
-				
-			}else if (choice == 3) {
-				//modifier une pizza
-				String codeModif;
-								
-				
+				//méthode pour ajouter nouvelle pizza
+
+				Pizza newPizza =new Pizza(code, nom, prix);	
+				pizzaDao.saveNewPizza(newPizza);
+
+			}else if (choice == 3) {											
 				System.out.println("Mise à jour d'une pizza");
-				
+
+				//méthode pour modifier une pizza
+
 				System.out.println("Veuillez saisir le code de la pizza à modifier : ");
-				codeModif = choiceMenu.nextLine();
-					
-				
+				String codeModif = choiceMenu.nextLine();
+				pizzaDao.findPizzaByCode(codeModif);
+
 				System.out.println("Veuillez écrire le nouveau code : ");
 				code = choiceMenu.nextLine();
 
@@ -97,14 +92,10 @@ public class PizzeriaAdminConsoleApp {
 				System.out.println("Veuillez écrire le nouveau prix : ");
 				prix = choiceMenu.nextDouble();
 
-				for (int i =0; i<tableauPizza.length; i++) {
-					if (codeModif.equals(tableauPizza[i].code)) {
-						tableauPizza[i].code = code;
-						tableauPizza[i].libelle = nom;
-						tableauPizza[i].prix = prix;
-					}
-				}
-				
+				Pizza newPizza = new Pizza(code, nom, prix);
+				pizzaDao.updatePizza(code, newPizza);
+
+
 			} else if (choice == 4) {
 				//supprimer une pizza
 
@@ -113,18 +104,9 @@ public class PizzeriaAdminConsoleApp {
 
 				String codeSuppr = choiceMenu.nextLine();
 
-				Pizza[] newTableauPizza = new Pizza[tableauPizza.length-1];
+				pizzaDao.deletePizza(codeSuppr);
 
-				int iTemp = 0;
 
-				for (int i = 0; i< tableauPizza.length; i++) {
-					if (!codeSuppr.equals(tableauPizza[i].code)) {
-						newTableauPizza[iTemp] = tableauPizza[i];
-						iTemp++;
-					}
-				}
-				tableauPizza = newTableauPizza ;
-				
 			}
 			else {
 				//quitter l'application						
